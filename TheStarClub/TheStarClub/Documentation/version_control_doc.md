66aca9fda9f34e1d29cfeaf8da2fd4000cf834c4
AugustinBunani <48664320+AugustinBunani@users.noreply.github.com>
2022-05-02 12:10:57 +1000

    1.0.0

---
+++ TheStarClub/TheStarClub/Documentation/version_control_doc.md
@@ -0,0 +1,7 @@
+----------------------------
+version : 1.0.0,
+Schema : 1
+Coder : Gus
+Date : 2/05/2022
+Changes : Initial Commit
+----------------------------
---
+++ TheStarClub/TheStarClub/View/TheStarClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStarClubApp.swift
+//  TheStarClub
+//
+//  Created by Augustin Bunani on 2/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheStarClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
